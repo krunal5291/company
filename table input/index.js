212ecdcbf503fc9d1aa05d05e38221c24530d92e
krunal5291 <krunalkalathiya11@gmail.com>
2024-09-16 14:21:05 +0530

    final

--- table input/index.js
+++ table input/index.js
@@ -4,25 +4,25 @@ function Total(element) {
     let sum = 0
 
     inputs.forEach(input => {
-        sum =sum+ Number(input.value)
+        sum = sum + Number(input.value)
     })
 
     row.querySelector('.total').innerText = sum
 }
 
 function ColumnTotal() {
-    let tbody = document.querySelector('tbody') 
+    let tbody = document.querySelector('tbody')
     let rows = tbody.querySelectorAll('tr')
     let columnSums = [0, 0, 0]
 
     rows.forEach(row => {
         let inp = row.querySelectorAll('input[type="number"]');
         inp.forEach((inp, i) => {
-            columnSums[i] =columnSums[i]+ Number(inp.value)
-        });
-    });
+            columnSums[i] = columnSums[i] + Number(inp.value)
+        })
+    })
 
     columnSums.forEach((sum, i) => {
-        document.getElementById(`col-total${i    + 1}`).innerText = sum
+        document.getElementById(`col-total${i + 1}`).innerText = sum
     })
 }
